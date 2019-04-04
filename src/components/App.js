2dae7899654e5d3e243136ab1a8534c57a3111f9
Beylul Kbreab <bkbreab8@gmail.com>
2019-04-03 19:02:43 -0700

    add header and router

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react'
+import { BrowserRouter, Route } from "react-router-dom";
 import StarWarIndex from './star-war-index'
+import CommentIndex from "./comment-index";
+import Header from './header'
 import './styles/App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
-        <header className='App-header'>Test page</header>
-        <div style={{ display: 'flex', justifyContent: 'center'}}>
-          <StarWarIndex />
+      <BrowserRouter className='App'>
+          <Header className='App-header' />
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <Route path='/' exact component={StarWarIndex} />
+          <Route path='/comment' component={CommentIndex} />
         </div>
-      </div>
+        </BrowserRouter>
     );
   }
 }
